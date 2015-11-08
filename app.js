bb3b9d5d4da8eb707fd55154fdc2644799f4e687
Cody Bouscaren <codybousc@gmail.com>
2015-11-08 09:21:30 -0500

    Adds dynamic route

--- app.js
+++ app.js
@@ -6,9 +6,22 @@ app.use(logger);
 
 app.use(express.static('public'));
 
-app.get('/blocks', function(request, response) {
-  var blocks = ["Fixed", "Movable", "Rotating"];
-  response.json(blocks);
+var blocks = {
+    "Fixed": "Fastened securely in position",
+    "Movable": "Capable of being moved",
+    "Rotating": "Moving in a circle around its center"
+};
+
+
+app.get('/blocks/:name', function(request, response) {
+  var description = blocks[request.params.name];
+  if (!description) {
+      response.status(404).json("No description found for " + request.params.name); 
+  }
+  else {
+      response.json(description);
+  }
+
 })
 
-app.listen(3000); 
+app.listen(3000);
